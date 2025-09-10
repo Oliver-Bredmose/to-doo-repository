886176890b2988b4637089479b374ed780295480
Oliver-Bredmose <oliver.bredmose@gmail.com>
2025-09-10 08:56:06 +0200

    some buttons funtion

--- To doo app/JS/Script.js
+++ To doo app/JS/Script.js
@@ -1,80 +1,91 @@
-
 // #region globals
 const contentsection = document.getElementById('content');
+const localStorageKey = "toDooAppStat";
 console.log(contentsection);
 
-let currentData=null;
+let currentData = null;
 //#endregion globals
 
 initapp()
 
 // #region MODEL CODE
-function getData(){
+function getData() {
     console.log('getData');
-return JSON.parse(localStorage.getItem("toDooApp_v1")) 
+    return JSON.parse(localStorage.getItem("toDooApp_v1"))
 }
 
-function saveData(myData){
+function saveData(myData) {
     console.log('saveData');
-    let serializedData=JSON.stringify(myData)
-
+    let serializedData = JSON.stringify(myData)
     localStorage.setItem("toDooApp_v1", serializedData)
 }
 
-function makeNewData(){
+function makeNewData() {
     console.log('makeNewData');
-// dummy data husk at tømme lister inden deployment
-    let NewData={
-DarkMode:false,
- list:[
-    {
-        listName: "liste 1",
-        items:[{name:"item 1",done:false},{name:"item 2",done:true},{name:"item 3",done:false}]
-    },
-    {
-        listName: "indkøb",
-        items:[{name:"Kød",done:false},{name:"salat",done:true},{name:"agurk",done:false}]
+    // dummy data husk at tømme lister inden deployment
+    let NewData = {
+        DarkMode: false,
+        list: [
+            {
+                listName: "liste 1",
+                items: [
+                    { name: "item 1", done: false },
+                    { name: "item 2", done: true },
+                    { name: "item 3", done: false }
+                ]
+            },
+            {
+                listName: "indkøb",
+                items: [
+                    { name: "Kød", done: false },
+                    { name: "salat", done: true },
+                    { name: "agurk", done: false }
+                ]
+            }
+        ]
     }
- ]  
-    }
-    
+
     return NewData;
 }
-
-
 //#endregion model code
 
 // #region CONTROLLER CODE
-function initapp(){
+function initapp() {
     console.log('initapp');
-    
-    //hent data
-    currentData=getData()
-    
-    //evaluer data 
 
-    if (currentData==null) {
-// vi har ikke data      
-    currentData=makeNewData()
-    saveData(currentData)
+    // hent data
+    currentData = getData()
+
+    // evaluer data 
+    if (currentData == null) {
+        // vi har ikke data      
+        currentData = makeNewData()
+        saveData(currentData)
     }
 
     // vi har data
     makeListsView(currentData)
+
+    // attach add button
+    document.getElementById("addButton").addEventListener("click", () => {
+        const newName = prompt("Enter name for new list:")
+        if (newName) {
+            currentData.list.push({ listName: newName, items: [] })
+            saveData(currentData)
+            makeListsView(currentData)
+        }
+    })
 }
-
-
 //#endregion controller code
 
 // #region VIEW CODE
-function listViewCallBack(action,index){
+function listViewCallBack(action, index) {
     console.log('listViewCallBack');
     console.log(action);
     console.log(index);
 
-    switch(action){
+    switch (action) {
         case "ShowList":
-            //showList(index)
             console.log(currentData.list[index])
             contentsection.innerHTML = ""
 
@@ -84,50 +95,54 @@ function listViewCallBack(action,index){
                 contentsection.appendChild(itemContainer)
             })
             break;
+
         case "EditList":
             editList(index)
             break;
+
         case "DeleteList":
             deleteList(index)
             break;
+
         default:
             console.log("no action");
     }
 }
-function makeListsView(data){
+
+function makeListsView(data) {
     console.log('makeListsView');
-    //tøm conrentsection
-    console.log(contentsection);
-    
-    contentsection.innerHTML='';
+    // tøm contentsection
+    contentsection.innerHTML = '';
 
-    data.list.forEach((list,index) => {
+    data.list.forEach((list, index) => {
+        let listContainer = document.createElement('div')
 
-        console.log(list.listName);
+        listContainer.innerHTML = `
+            <h2 onclick="listViewCallBack('ShowList', ${index})">${list.listName}</h2>
+            <button onclick="listViewCallBack('EditList', ${index})">Edit</button>
+            <button onclick="listViewCallBack('DeleteList', ${index})">Delete</button>
+        `
 
-        let listContainer=document.createElement('div')
-//vis liste
-listContainer.innerHTML=`<h2 onclick="listViewCallBack('ShowList',${index})">${list.listName}
-</h2><button>edit</button>
-</h2><button>Delete</button>`
-
-contentsection.appendChild(listContainer)
- 
+        contentsection.appendChild(listContainer)
     });
-
-
 }
 
-//BUTTONS
+// EDIT LIST
+function editList(index) {
+    const newName = prompt("Enter new list name:", currentData.list[index].listName)
+    if (newName) {
+        currentData.list[index].listName = newName
+        saveData(currentData)
+        makeListsView(currentData)
+    }
+}
 
-//DELETE BUTTON
-function DeleteList(index) {
-    if (!confirm("Are you sure?")) {
+// DELETE LIST
+function deleteList(index) {
+    if (confirm("Are you sure?")) {
         currentData.list.splice(index, 1)
         saveData(currentData)
         makeListsView(currentData)
     }
 }
 //#endregion view code
-
-
